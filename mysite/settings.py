c217eda0760a1e7d9d15858c57d144562977bef3
EmperorOwl <raylin9703@gmail.com>
2024-01-12 14:54:07 +1100

    Add deployment settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,10 @@
+import os
+import platform
+from dotenv import load_dotenv
 from pathlib import Path
+
+# Load env file.
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -7,12 +13,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@myo&mxq9xum=np-cx_5k4i)y_np0^7ole72@mcjh)n8s%$_-_'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-@myo&mxq9xum=np-cx_5k4i)y_np0^7ole72@mcjh)n8s%$_-_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if platform.node() == 'VPS' else True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1',  # For development server.
+    '162.248.101.207', 'app.raylin.dev'  # For production server.
+]
 
 # Application definition
 
@@ -98,7 +107,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'velo-static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
